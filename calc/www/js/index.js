0a73a3f4715123dd6c32734a85f9c1036140d452
bhagatsingh1998 <bhagatsingh.17is@saividya.ac.in>
2020-10-05 16:52:47 +0530

    added readme and padding at #disply

--- calc/www/js/index.js
+++ calc/www/js/index.js
@@ -1,24 +1,19 @@
 var num = document.getElementById("display");
 
-function calc(obj) 
-{
-    var pushed = obj.innerHTML;
-    
-    if(pushed == "=") {
-        num.innerHTML = eval(num.innerHTML);   
+function calc(obj) {
+  var pushed = obj.innerHTML;
+
+  if (pushed == "=") {
+    num.innerHTML = eval(num.innerHTML);
+  } else if (pushed == "AC") {
+    num.innerHTML = "0";
+  } else if (pushed == "DEL") {
+    num.innerHTML = num.innerHTML.substr(0, num.innerHTML.length - 1);
+  } else {
+    if (num.innerHTML == "0") {
+      num.innerHTML = pushed;
+    } else {
+      num.innerHTML = num.innerHTML + pushed;
     }
-    else if(pushed == "AC") {
-        num.innerHTML = "0";
-    }
-    else if(pushed == "DEL") {
-        num.innerHTML = num.innerHTML.substr(0, num.innerHTML.length - 1);
-    }
-    else {
-        if(num.innerHTML == "0") {
-            num.innerHTML = pushed;
-        }
-        else {
-            num.innerHTML = num.innerHTML + pushed;
-        }
-    }   
+  }
 }
